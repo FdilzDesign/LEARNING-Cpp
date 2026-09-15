78134160e60d4909c0eb6e15354b1bb02760a3a8
FdilzDesign <iseffadilah7@gmail.com>
2026-09-15 22:15:14 +0700

    Tugas Perkenalan C++ Algoritma

---
+++ io.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() { int n;
+cout << "value:";
+cin >>n;
+cout <<"value n:"<<n<<endl;
+
+
+    return 0;
+}
+
+//cout:menampilkan kode yg ingin dieksekusi
+//cin:memasukan nilai dalam program
+//return:memberikan nilai
+//int main:corenya cpp gaboleh ada 2 dalam satu kode
---
+++ tipedata.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() { 
+    int value = 10;//nilai bulat
+    double ipk = 0.5;//nilai desimal
+    bool lulus = true;//ini adalah boolean 
+    char predikat = 'A';//hanya menyimpan 1 nilai karakter
+    string nama = "Fadil";
+    unsigned int n1 = -1;
+    nama.append(" Isef");//1.alternatif cara ke 1 nambahin nama 
+    nama += " Isef Fadil";//2.alternaatif cara ke 2 nambahin nama 
+
+    cout << "Nama gw: "<< nama <<endl;
+    cout << "Nilai GW: "<< value <<endl;
+    cout << "Nilai IPK Saya: "<< ipk <<endl;
+    cout << " Predikat: "<< predikat <<endl;
+    cout << "nilai: "<< n1 <<endl;
+
+    if (lulus) {
+        cout << "Saya Lulus" <<endl;
+    } else {
+        cout << "Saya Ga Lulus" <<endl;
+    }
+
+    return 0;
+}
